1e90f19ef8b7af83ac9692361c974f6566aafee4
JareddZwick <zwickkids@hotmail.com>
2022-03-31 20:50:05 -0700

    added guide route

--- mongoexport.py
+++
@@ -1,7 +0,0 @@
-let url = 'https://api.sheety.co/1dd8818803e588fc51a17ec92c66018b/copyOfAlgorithmDemo/collegeData';
-fetch(url)
-.then((response) => response.json())
-.then(json => {
-  // Do something with the data
-  console.log(json.collegeData);
-});
